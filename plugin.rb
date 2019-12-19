4ecba2f65fc45b7335db1dd2d7ce28bcbf97f8e1
XANT3R <xan96@outlook.com>
2019-12-19 18:42:17 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-sidebar-blocks
 # about: add a sidebar to topic lists (discovery) with several blocks available: posts from a category, recent replies
-# version: 0.1
+# version: 0.2
 # authors: pmusaraj
 # url: https://github.com/pmusaraj/discourse-home-sidebar
 
